40bea13570f95d264b565a9d62af5b2a8c73e148
ahallam <alexhallam75@gmail.com>
2019-12-08 13:17:29 -0500

    API fixed, Header and HeroImage styling

--- src/config.js
+++ src/config.js
@@ -3,8 +3,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = process.env.API_KEY;
-// const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
+const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
 
 const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 // Sizes: w300, w780, w1280, original
